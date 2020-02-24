851ab3b352c2da1ff6af958d8e266090569cf6f2
Andri Sedania <andri@sedaniainnovator.com>
2020-02-24 10:42:49 +0700

    Simulate Alarm with Toastr

--- resources/views/layouts/asap/script.blade.php
+++ resources/views/layouts/asap/script.blade.php
@@ -4,3 +4,4 @@
 <script src="{{ asset('frontend/plugins/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
 <!-- AdminLTE App -->
 <script src="{{ asset('frontend/dist/js/adminlte.js') }}"></script>
+<script src="{{ asset('frontend/plugins/toastr/toastr.min.js') }}"></script>
